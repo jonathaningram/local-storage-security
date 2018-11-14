d6eb06a60dc8cc89387f3d86b873197295ce62ad
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2018-11-14 14:32:26 +1100

    Make it easier to see attacker's website

--- app/public/app.js
+++ app/public/app.js
@@ -1,6 +1,7 @@
 // Simulate setting the token once the user successfully logs in.
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+const token = Math.random().toString();
 
 localStorage.setItem("token", token);
+
+document.getElementById("user_token").textContent = token;
